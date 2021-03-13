92d4ce931bcabbf69d5b797b04b3db4701a98d15
jp <jpmartel98@gmail.com>
2021-03-13 04:44:06 +0100

    itwork

--- rush.c
+++ rush.c
@@ -12,35 +12,61 @@
 
 #include "unistd.h"
 
-void	ft_print_widht(int squar_witdh)
+void ft_print_fn(char chared)
 {
-	char	corner;
-	char	iterator;
-	char	height;
-	char	space;
-	iterator = 0;
-	space = '\n';
-	height = 'b';
-	corner = 'a';
-	while (iterator <= squar_witdh )
-	{
-		if(iterator == 0  || iterator == squar_witdh) 
-		{	
-			write(1, &corner, 1);
-			write(1, &height, 1);
-		}
-		else {
-			write(1, &height, 1);
+  write(1,&chared,1);
+}
 
-		}
+
+void	ft_print_middle(int squar_witdh)
+{
+
+
+int iterator;
+iterator = 0;
+while(iterator++  <= squar_witdh  )
+{
+
+  if(iterator  == 0   &&  iterator  == squar_witdh)
+  {
+      ft_print_fn(32);
+
+  }
+
+  else if (iterator == squar_witdh)
+  {
+    ft_print_fn('\n');
+  } 
+
+    else
+    {
+      ft_print_fn('r');
+    }
+
+}
+}
+
+
+//we could make it a fn so we only need one fn with params ex a ' ' or b a
+void	ft_print_top(int squar_witdh)
+{
+	int iterator;
+	iterator = 1;
+      ft_print_fn('a');
+	while (iterator  <= squar_witdh )
+	{
+    ft_print_fn('b');
 		++iterator;
 	}
+
+      ft_print_fn('a');
+      ft_print_fn('\n');
 }
 
 int main(void)
 
 {
-ft_print_widht(20);
-
-ft_print_widht(20);
+ft_print_top(20);
+ft_print_top(20);
+ft_print_middle(20);
 }
