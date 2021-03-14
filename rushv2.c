813607d8c94b43799173a9f097f5a392177c96ab
Jean-philippe Martel <jemartel@c2r5p3.42quebec.com>
2021-03-14 15:24:28 -0400

    indentended

--- rushv2.c
+++ rushv2.c
@@ -1,64 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rushv2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jean-phil <jemartel@student.42quebec>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/14 15:01:11 by jean-phil         #+#    #+#             */
+/*   Updated: 2021/03/14 15:23:47 by jean-phil        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "unistd.h"
 
-void ft_print_fn(char chared)
+void 	ft_print_fn(char chared)
 {
-  write(1,&chared,1);
+	write(1, &chared, 1);
 }
 
-
-
-void	ft_print_top(int squar_witdh,char corner,char center)
+void	ft_print_top(int squar_witdh, char corner, char center)
 {
-	int iterator;
+	int		iterator;
+
 	iterator = 1;
-      ft_print_fn(center);
-	while (++iterator  <= squar_witdh - 1)
-		{ 	
-    		ft_print_fn(corner);
-
-			}
-
-	if(squar_witdh > 1)
-	{
-   		ft_print_fn(center);
+	ft_print_fn(center);
+	while (++iterator <= squar_witdh - 1)
+	{ 	
+		ft_print_fn(corner);
 	}
-
-      		ft_print_fn('\n');
-   		}
-
-
-void fit_replcate_middle(int times,int widht)
-{
-int iterator = 1;
-while(iterator < times )
-{
-iterator++;
-ft_print_top(widht ,' ','|');
+	if (squar_witdh > 1)
+	{
+		ft_print_fn(center);
+	}
+	ft_print_fn('\n');
 }
+
+void	fit_replcate_middle(int times, int widht)
+{
+	int		 iterator;
+
+	iterator = 1;
+	while (iterator < times)
+	{
+		iterator ++;
+		ft_print_top(widht, ' ', '|');
+	}
 }
-void rush(widht,heigh)
-{
 
-if(heigh > 0  && widht> 0)
+void	rush(widht, heigh)
 {
-
-
-if(heigh == 1  && heigh == 1)
-{
-ft_print_top(widht ,'-','o');
-}	
-else if(heigh >= 1)
-{
-ft_print_top(widht,'-','o');
-fit_replcate_middle(heigh - 1,widht);
-ft_print_top(widht,'-','o');
-}
-}
-}
-int main(void)
-{
-rush(5,5);
-
-
+	if (heigh > 0 && widht > 0)
+	{
+		if (heigh == 1 && heigh == 1)
+		{
+			ft_print_top(widht, '-', 'o');
+		}	
+		else if (heigh >= 1)
+		{
+			ft_print_top(widht, '-', 'o');
+			fit_replcate_middle(heigh - 1, widht);
+			ft_print_top(widht, '-', 'o');
+		}
+	}
 }
