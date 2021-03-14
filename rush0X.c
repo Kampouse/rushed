616521162e78d46485a1a9d661d3737e24426495
Jean-philippe Martel <jemartel@c2r5p3.42quebec.com>
2021-03-14 15:48:49 -0400

    worklikeacharm

--- rush0X.c
+++ rush0X.c
@@ -6,13 +6,13 @@
 /*   By: jean-phil <jemartel@student.42quebec>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/14 15:01:11 by jean-phil         #+#    #+#             */
-/*   Updated: 2021/03/14 15:23:47 by jean-phil        ###   ########.fr       */
+/*   Updated: 2021/03/14 15:43:13 by jean-phil        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "unistd.h"
 
-void 	ft_print_fn(char chared)
+void 	ft_putchar(char chared)
 {
 	write(1, &chared, 1);
 }
@@ -22,16 +22,16 @@ void	ft_print_top(int squar_witdh, char corner, char center)
 	int		iterator;
 
 	iterator = 1;
-	ft_print_fn(center);
+	ft_putchar(center);
 	while (++iterator <= squar_witdh - 1)
 	{ 	
-		ft_print_fn(corner);
+		ft_putchar(corner);
 	}
 	if (squar_witdh > 1)
 	{
-		ft_print_fn(center);
+		ft_putchar(center);
 	}
-	ft_print_fn('\n');
+	ft_putchar('\n');
 }
 
 void	fit_replcate_middle(int times, int widht)
